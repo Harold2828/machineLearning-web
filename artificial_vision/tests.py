cd86d9656d86f729de5b6d959b3f7788f3513074
Harold2828 <harold2828@hotmail.com>
2022-09-05 21:16:02 -0500

    ORIGIN -m first testing

--- artificial_vision/tests.py
+++ artificial_vision/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from . models import Post
+
 
 # Create your tests here.
+class ModelTesting(TestCase):
+    def setUp(self):
+        self.artifial_vision = Post.objects.create(title='django',
+                                                   author='django',
+                                                   slug='django')
+
+    def test_post_model(self):
+        d = self.artifial_vision
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'django')
